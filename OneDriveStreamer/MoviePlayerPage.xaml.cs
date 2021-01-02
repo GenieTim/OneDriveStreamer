0ee8f66fa55fe920cbbdade0f81880da5bbebc61
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-01-02 22:04:33 +0100

    Initial commit with working code

---
+++ OneDriveStreamer/MoviePlayerPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using OneDriveStreamer.Utils;
+using System.IO;
+using Windows.Media.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using MimeTypes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OneDriveStreamer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MoviePlayerPage : Page
+    {
+        public MoviePlayerPage()
+        {
+            this.InitializeComponent();
+        }
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var parameters = (VideoNavigationParameter)e.Parameter;
+            var client = parameters.oneDriveClient;
+            var builder = client.Drive.Root.ItemWithPath(parameters.videoPath);
+            var file = await builder.Request().GetAsync();
+            Stream contentStream = await builder.Content.Request().GetAsync();
+            string mimeType = MimeTypeMap.GetMimeType(file.Name);
+            mediaPlayer.Source = MediaSource.CreateFromStream(contentStream.AsRandomAccessStream(), mimeType);
+            // parameters.Name
+            // parameters.Text
+            // ...
+        }
+    }
+
+}
---
+++ OneDriveStreamer/Utils/AppConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Application­Model;
+
+namespace OneDriveStreamer.Utils
+{
+    class AppConfig
+    {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public AppConfig()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Package.Current.InstalledLocation.Path)
+            .AddJsonFile("appsettings.json", optional: false);
+
+            _configurationRoot = builder.Build();
+        }
+
+        private T GetSection<T>(string key) => _configurationRoot.GetSection(key).Get<T>();
+        public string GetClientID()
+        {
+            return GetSection<string>("clientId");
+        }
+    }
+}
